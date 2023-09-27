5d474389c18f18be8d2e5e7366eae8232ddc4ba9
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-09-27 20:12:24 +0000

    Python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,21 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Michael B
+Created on: Sep 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
+# our variavbles
+temperatureCelsius = 0
+temperatureKelvin = 0
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        temperatureCelsius = input.temperature()
+        temperatureKelvin = temperatureCelsius + 173.15
+        temperatureKelvin = round(temperatureKelvin)
+        display.show(("The temperature is:") + (str(temperatureKelvin)) + ("K."))
